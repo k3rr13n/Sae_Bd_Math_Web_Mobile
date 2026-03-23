c7f4c9bcaf6553302416fbb15cbbc0bea13ab63c
AsmaH <asma.hachelef1@gmail.comgit config --listgit config --global user.email asma.hachelef1@gmail.com>
2026-03-23 18:26:17 +0100

    début api_models

--- Api_vol/App/api_models.py
+++ Api_vol/App/api_models.py
@@ -2,3 +2,20 @@ from flask_restx import fields
 from .extensions import api
 
 
+compagnie_model = api.model('Compagnie', {
+    "nom_compagnie": fields.String
+})
+
+compagnie_input_model = api.model('CompagnieInput', {
+    "nom_compagnie":fields.String
+})
+
+aeroport_model = api.model("Aeroport", {
+    "nom_aeroport": fields.String,
+    "ville_id": fields.Integer #a revoir
+})
+
+# aeroport_input_model = api.model("AeroportInput",  {
+#     "nom_aeroport": fields.String,
+#     "ville_id": fields.Integer #a revoir
+# })
